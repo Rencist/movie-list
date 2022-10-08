a958f22a3f77ee3bc336f7a283378d48a1d5fd6f
Rencist <dafianakhdan@gmail.com>
2022-10-08 18:47:52 +0700

    feat: Movies components, loading

---
+++ src/Context.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+const AppContext = React.createContext();
+
+const allWaifusUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomWaifuUrl = 'https://www.themealdb.com/api/json/v1/1/random.php/';
+
+const AppProvider = ({ children }) => {
+  const [waifus, setWaifus] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchWaifus = async (url) => {
+    setLoading(true);
+    try {
+      const { data } = await axios(url);
+      if (data.meals) {
+        setWaifus(data.meals);
+      } else {
+        setWaifus([]);
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchWaifus(allWaifusUrl);
+  }, []);
+
+  return <AppContext.Provider value={{ loading, waifus }}>{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/components/Modal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Modal = () => {
+  return <div className="m-12 bg-slate-600 rounded-full text-center hover:bg-slate-700">Hello Modal</div>;
+};
+
+export default Modal;
